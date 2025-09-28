aa84ae72934dfdf8e3bb6ebe5e5a5a1492c884df
gokul2736 <gokulm2732006@gmail.com>
2025-09-28 15:49:05 +0530

    Changes made in applicationa definition

--- source/petcare/settings.py
+++ source/petcare/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    'django.contrib.staticfiles',
     'shop',
 ]
 
@@ -122,3 +121,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+STATICFILES_DIRS = [BASE_DIR / "static"]
